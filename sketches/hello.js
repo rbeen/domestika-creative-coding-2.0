7622fd8e2752d1215231d9a73694492b7ed5145d
Rob Been <rob@two-legs.com>
2024-09-08 14:49:45 +0200

    adds convenient run command

--- sketches/hello.js
+++ sketches/hello.js
@@ -6,8 +6,14 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'black';
+    context.fillStyle = 'lightgrey';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = "rgb(200 0 0)";
+    context.fillRect(10, 10, 50, 50);
+
+    context.fillStyle = "rgb(0 0 200 / 50%)";
+    context.fillRect(30, 30, 50, 50);
   };
 };
 
