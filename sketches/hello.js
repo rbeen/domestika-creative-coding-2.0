a56e891d23ca3493acdbb80f75923014541bb290
Rob Been <rob@two-legs.com>
2024-09-08 15:41:42 +0200

    formats js files using prettier

--- sketches/hello.js
+++ sketches/hello.js
@@ -1,9 +1,9 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 800, 800 ],
+  dimensions: [800, 800],
   animate: true,
-  fps: 60
+  fps: 60,
 };
 
 const sketch = ({ context, width, height }) => {
@@ -11,7 +11,7 @@ const sketch = ({ context, width, height }) => {
   let x = 0;
   let y = 0;
 
-  context.fillStyle = 'lightgrey';
+  context.fillStyle = "lightgrey";
   context.fillRect(0, 0, width, height);
 
   return ({ context, width, height, frame }) => {
@@ -23,22 +23,22 @@ const sketch = ({ context, width, height }) => {
       x = 0;
     }
 
-    context.fillStyle = (frame%2) ? 'black':'white';
+    context.fillStyle = frame % 2 ? "black" : "white";
     context.strokeStyle = context.fillStyle;
 
     context.lineWidth = 10;
 
     // Wall
-    context.strokeRect(x+75, y+140, 150, 110);
+    context.strokeRect(x + 75, y + 140, 150, 110);
 
     // Door
-    context.fillRect(x+130, y+190, 40, 60);
+    context.fillRect(x + 130, y + 190, 40, 60);
 
     // Roof
     context.beginPath();
-    context.moveTo(x+50, y+140);
-    context.lineTo(x+150, y+60);
-    context.lineTo(x+250, y+140);
+    context.moveTo(x + 50, y + 140);
+    context.lineTo(x + 150, y + 60);
+    context.lineTo(x + 250, y + 140);
     context.closePath();
     context.stroke();
   };
