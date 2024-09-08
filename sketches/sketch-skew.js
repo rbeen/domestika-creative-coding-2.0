00f0187e368e6b2f4d31864f4b98bc3cd2a7e330
Rob Been <rob@two-legs.com>
2024-09-08 16:08:33 +0200

    draws 20 skewed rectangles
    fixes angling and position issues in drawSkewedRect

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch");
 const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -7,29 +8,36 @@ const settings = {
 
 const sketch = () => {
   // parameters for drawing our rectangle
-  let x, y;
+  let x, y, w, h;
+
+  const numberOfRectangles = 20;
+  const degrees = -30;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    x = width * 0.5;
-    y = height * 0.5;
+    for (let i = 0; i < numberOfRectangles; i++) {
+      x = random.range(0, width);
+      y = random.range(0, height);
+      w = random.range(200, 600);
+      h = random.range(40, 200);
+      // save context before manipulation
+      context.save();
 
-    // save context before manipulation
-    context.save();
+      // manipulate context coordinates so we can draw more cleanly
+      context.translate(x, y);
 
-    // manipulate context coordinates so we can draw more cleanly
-    context.translate(x, y);
+      drawSkewedRect({
+        context,
+        w,
+        h,
+        degrees,
+      });
 
-    drawSkewedRect({
-      context,
-      x,
-      y,
-    });
-
-    // go back to our previous context
-    context.restore();
+      // go back to our previous context
+      context.restore();
+    }
   };
 };
 
@@ -39,7 +47,7 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
   const ry = Math.sin(angle) * w;
 
   context.save();
-  context.translate(rx * -0.5, ry * -0.5);
+  context.translate(rx * -0.5, (ry + h) * -0.5);
 
   // draw a blue outline rectangle edge by edge
   context.strokeStyle = "blue";
